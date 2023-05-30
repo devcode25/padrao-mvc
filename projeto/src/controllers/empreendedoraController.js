dd36b34a1bf11950c0f5e823da6cbda1db1c2005
devcode25 <tereza.dev@outlook.com>
2023-05-30 12:25:33 -0300

    Criamos as rotas GET, POST, PUT E DELETE

--- projeto/src/controllers/empreendedoraController.js
+++ projeto/src/controllers/empreendedoraController.js
@@ -9,6 +9,18 @@ class EmpreendedoraController {
     })
   }
 
+  static getByIdEmpreendedoras = (req, res) => {
+    const id = req.params.id;
+
+    empreendedoras.findById(id, (err, empreendedoras) => {
+      if(err) {
+        res.status(400).send({message: `${err.message} - Id da empreendedora não localizado.`})
+      } else {
+        res.status(200).send(empreendedoras);
+      }
+    })
+  }
+
   static createEmpreendedoras = (req, res) => {
     let empreendedora = new empreendedoras(req.body);
   
@@ -17,6 +29,30 @@ class EmpreendedoraController {
         res.status(500).send({ message: `${err.message} - falha ao cadastrar empreendedora` })
       } else {
         res.status(201).send(empreendedora.toJSON())
+      }
+    })
+  }
+
+  static updateEmpreendedoras = (req, res) => {
+    const id = req.params.id;
+
+    empreendedoras.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(!err) {
+        res.status(200).send({message: 'Empreendedora atualizada com sucesso'})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
+  
+  static deleteEmpreendedoras = (req, res) => {
+    const id = req.params.id;
+
+    empreendedoras.findByIdAndDelete(id, (err) => {
+      if(!err){
+        res.status(200).send({message: 'Empreendedora removida com sucesso'})
+      } else {
+        res.status(500).send({message: err.message})
       }
     })
   }
--- projeto/src/routes/empreendedora.js
+++ projeto/src/routes/empreendedora.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router
 .get("/", EmpreendedoraController.getAllEmpreendedoras)
 .post("/", EmpreendedoraController.createEmpreendedoras)
+.put("/:id", EmpreendedoraController.updateEmpreendedoras)
+.delete("/:id", EmpreendedoraController.deleteEmpreendedoras)
+.get("/:id", EmpreendedoraController.getByIdEmpreendedoras)
 
 module.exports = router;
